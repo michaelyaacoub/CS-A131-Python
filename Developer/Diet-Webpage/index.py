aa92569c6783c9cabe9757e9888e813b2d0382cd
Michael Yaacoub <michael.yaacoub7@gmail.com>
2023-01-01 23:27:32 -0800

    first commin in 23

---
+++ Developer/Diet-Webpage/index.py
@@ -0,0 +1,31 @@
+# name = input("Name: ")
+# print(f"Hello, {name}")
+
+# num = int(input("Number: "))
+# if num > 0:
+#     print("num is positive")
+# elif num < 0:
+#     print("num is negative")
+# else:
+#     print("num is zero")
+
+# Define a list of names
+names = ["Jullie", "Michael", "Joe", "Mark", "Ben"]
+
+names.append("Draco")
+names.sort()
+print(names)
+for i in names:
+    print(i)
+
+# Ceate an empty set
+# s = set()
+
+# # add element
+# s.add(1)
+# s.add(2)
+# s.add(3)
+# s.add(4)
+# s.add(3)
+
+# print(f"this set has {len(s)} elements")
