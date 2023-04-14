90dff222fce896f1d347f7bdc68b606d13e6c2d0
Michael Yaacoub <56051759+michaelyaacoub@users.noreply.github.com>
2023-04-13 23:35:20 -0700

    Delete Desktop/CS-131-Py directory

--- Desktop/CS-131-Py/Python Assignments/A131_EX02_MICHAEL_YAACOUB/powers.py
+++
@@ -1,9 +0,0 @@
-# --------------------------------------
-# Name: Michael Yaacoub
-# Date: 02/08/2023
-# Exercise 2, part C
-# --------------------------------------
-
-print(f'6 squared is {6 * 6}')      # Print 6 squared
-print(f'6 cubed is {6 * 6 * 6}')    # Print 6 cubed
-print(f'6 to the fourth power is {6 ** 4}') # Print 6 to 4th pow
