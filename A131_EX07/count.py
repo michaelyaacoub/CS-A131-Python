16b9bddb04b41063c76c0849742cf672874aa779
Michael Yaacoub <michael.yaacoub7@gmail.com>
2023-04-13 23:23:21 -0700

    EX07-A

---
+++ A131_EX07/count.py
@@ -0,0 +1,35 @@
+"""
+-----------------------------------
+Name: Michael Yaacoub
+Date: Apr/15/23
+Assignment: EX07 - Part A
+-----------------------------------
+Write a program that asks the user for a file name 
+and prints the number of characters, words, and lines in that file.
+"""
+
+
+def get_file(file):
+    """ Ask user for file name """
+    file = input("Enter file name: ")
+    pass
+
+def count_lines():
+    """ Counts the lines in a file. """
+    pass
+
+def count_words():
+    """ Counts the words in a file. """
+    pass
+
+def count_characters():
+    """ Counts the characters in a file. """
+    pass
+
+if __name__ == '__main__':
+    get_file("runfile.txt")
+    count_lines()
+    count_words()
+    count_characters()
+
+    print("Success!")
