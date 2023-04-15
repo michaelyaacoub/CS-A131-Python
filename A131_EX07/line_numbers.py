27be4248219d06deaf0c9b7000ad16ff7520ea5a
Michael Yaacoub <michael.yaacoub7@gmail.com>
2023-04-15 03:02:09 -0700

    Part B completed

---
+++ A131_EX07/line_numbers.py
@@ -0,0 +1,24 @@
+"""
+-------------------------------
+Name: Michael Yaacoub
+Date: Apr/15/2023
+Assignemnt: EX07 - Part B
+-------------------------------
+Write a program that reads a file containing text. 
+Read each line and send it to the output file, 
+preceded by line numbers.
+"""
+
+def number_lines():
+    """ Reads a file. """
+    filename = input("Enter a file name: ")
+    output_filename = input("Enter the output file name: ")
+    with open(filename, 'r') as infile, open(output_filename, "w") as outfile:
+        count_lines = 0
+        for line in infile:
+            if line != "":
+                count_lines = count_lines + 1
+            print("/* " + str(count_lines) + "*/ " + line.strip())
+            outfile.write("/*" + str(count_lines) + "*/" + line)
+
+number_lines()
