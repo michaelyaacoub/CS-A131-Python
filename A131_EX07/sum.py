7b9b058b2dff2aa65c7f123ab21da97fe6e6bf09
Michael Yaacoub <michael.yaacoub7@gmail.com>
2023-04-15 03:15:56 -0700

    Wrote the algorithm for part C

---
+++ A131_EX07/sum.py
@@ -0,0 +1,26 @@
+"""
+-----------------------------
+Name: Michael Yaacoub
+Date: Apr/16/23
+Assignment: EX07 - Part C
+-----------------------------
+Write a program that asks the user to input a set of floating-point values. 
+When the user enters a value that is not a number, 
+give the user a second chance to enter the value. After two chances, 
+quit reading input. 
+Add all correctly specified values and print 
+the sum when the user is done entering data. 
+Use exception handling to detect improper inputs.
+"""
+
+def sum():
+    """ Asks user for input and checks for validation after two prompts. """
+    # ask user for input
+    # use a try catch to ask them to re-enter for couple times.
+    # after second chance exit and sum up the entered values.
+    # initialize sum with zero '0' and return it if not proper input.
+    # else return sum.
+    # loop through the input.
+    
+
+    pass
